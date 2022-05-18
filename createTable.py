3add2b78451c690590e34e645c707d51fb251d9f
André Almeida <andrealmeidaa00@outlook.com>
2022-05-18 16:13:21 -0300

    tabelas criadas

--- createTable.py
+++ createTable.py
@@ -5,15 +5,77 @@ bancoDados = bancoDados.connect("bancoDados.db")
 bancoDados.execute("PRAGMA foreign_keys=on")
 cursor = bancoDados.cursor()
 
-createTable = """CREATE TABLE hospital(
-                cnpj VARCHAR(11) NOT NULL,
-                nome VARCHAR(20) NOT NULL,
-                rua VARCHAR(50) NOT NULL,
-                bairro VARCHAR(20) NOT NULL,
-                cidade VARCHAR(20) NOT NULL,
-                cep VARCHAR(8) NOT NULL,
-                telefone VARCHAR(10),
-                PRIMARY KEY(cnpj)
-            );
-            
-            """
+try:
+    createTable = """CREATE TABLE IF NOT EXISTS hospital(
+                    cnpj VARCHAR(11) NOT NULL,
+                    nome VARCHAR(20) NOT NULL,
+                    rua VARCHAR(50) NOT NULL,
+                    bairro VARCHAR(20) NOT NULL,
+                    cidade VARCHAR(20) NOT NULL,
+                    cep VARCHAR(8) NOT NULL,
+                    telefone VARCHAR(10),
+                    PRIMARY KEY(cnpj)
+                );
+                CREATE TABLE medico(
+                    crm VARCHAR(10) NOT NULL,
+                    cpf VARCHAR(11) NOT NULL,
+                    nome VARCHAR(50) NOT NULL,
+                    rua VARCHAR(50) NOT NULL,
+                    bairro VARCHAR(20) NOT NULL,
+                    cidade VARCHAR(20),
+                    cep VARCHAR(8) NOT NULL,
+                    especialidade TEXT NOT NULL,
+                    PRIMARY KEY(crm)
+                );
+                CREATE TABLE especialidade(
+                    cod_esp INTEGER NOT NULL,
+                    especialidade TEXT NOT NULL,
+                    crm VARCHAR(10),
+                    PRIMARY KEY(cod_esp AUTOINCREMENT),
+                    FOREIGN KEY(crm)
+                        REFERENCES medico(crm)
+                );
+                CREATE TABLE telefone(
+                    cod_tel INTEGER NOT NULL,
+                    contato1 VARCHAR(10),
+                    contato2 VARCHAR(10),
+                    crm VARCHAR(10) NOT NULL,
+                    PRIMARY KEY(cod_tel AUTOINCREMENT),
+                    FOREIGN KEY(crm)
+                        REFERENCES medico(crm)
+                );
+                CREATE TABLE enfermeira(
+                    coren VARCHAR(10),
+                    cpf VARCHAR(11),
+                    nome VARCHAR(50),
+                    rua VARCHAR(50),
+                    bairro VARCHAR(20),
+                    cidade VARCHAR(20),
+                    cep VARCHAR(8),
+                    PRIMARY KEY(coren)
+                );
+                    CREATE TABLE paciente(
+                    cpf VARCHAR(11) NOT NULL,
+                    rg VARCHAR(10) NOT NULL,
+                    nome VARCHAR(50) NOT NULL,
+                    rua VARCHAR(50) NOT NULL,
+                    bairro VARCHAR(20) NOT NULL,
+                    cidade VARCHAR(20) NOT NULL,
+                    cep VARCHAR(8) NOT NULL,
+                    PRIMARY KEY(cpf)
+                );
+                    CREATE TABLE tratamento(
+                    cod_tratamento VARCHAR(4),
+                    crm VARCHAR(10),
+                    data TEXT NOT NULL,
+                    PRIMARY KEY(cod_tratamento)
+                );"""
+
+    bancoDados.executescript(createTable)
+    bancoDados.commit()
+    print("\n\033[1;32mTABELAS FORAM CRIADAS COM SUCESSO\033[m\n")
+    cursor.close()
+    bancoDados.close()
+
+except bancoDados.DatabaseError or bancoDados.OperationalError as erro:
+    print(f"\n\033[1;31mERRO AO CRIAR TABELAS. ERRO:{erro}\033[m\n")
