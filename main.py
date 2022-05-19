c3def23699f587a8f50277338ae9f43555f532a4
André Almeida <andrealmeidaa00@outlook.com>
2022-05-19 20:26:50 -0300

    criando arquivo principal

---
+++ main.py
@@ -0,0 +1,18 @@
+def linha(tam=42):
+    print("_" * tam, end="")
+    print("\n")
+
+def cabecalho(txt):
+    linha()
+    print(txt.center(42))
+    linha()
+
+def menu(listOpcoes):
+    cabecalho("CADASTRO")
+    cont = 1
+    for item in listOpcoes:
+        print(f"{cont} - \033[34m{item}\033[m", end="")
+        print("\n")
+        cont += 1
+
+menu(["HOSPITAL", "MÉDICO", "PACIENTE"])
