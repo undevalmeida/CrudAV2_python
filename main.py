b6af9cb3bfa4eb7e1d89fada18f080a654d676c4
André Almeida <andrejunior573@gmail.com>
2022-05-19 21:36:19 -0300

    opções para cadastro de hospital

--- main.py
+++ main.py
@@ -11,12 +11,22 @@ def menu(listOpcoes):
     cabecalho("CADASTRO")
     cont = 1
     for item in listOpcoes:
-        print(f"{cont} - \033[34m{item}\033[m", end="")
+        print(f"{cont} - \033[1;33m{item}\033[m", end="")
         print("\n")
         cont += 1
 
 menu(["HOSPITAL", "MÉDICO", "PACIENTE"])
 
-# INTERAÇÃO COM USUÁRIO
-
+# INTERAÇÃO COM USUÁRIO PARA CADASTRO DE HOSPITAL
 linha()
+
+opcao = int(input("DIGITE SUA ESCOLHA: "))
+
+if opcao == 1:
+    cnpj = int(input("\033[1;33mNÚMERO DO CNPJ:\033[m] "))
+    nome = input("NOME DO HOSPITAL: ")
+    rua = input("RUA: ")
+    bairro = input("BAIRRO: ")
+    cidade = input("CIDADE: ")
+    cep = input("CEP: ")
+
